564a3f5b98c98b54a009994f680c4ba3d3341e43
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-28 13:45:10 +0300

    1.8

--- src/main/java/com/zaurtregulov/spring/spring_introduction/Person.java
+++ src/main/java/com/zaurtregulov/spring/spring_introduction/Person.java
@@ -3,13 +3,23 @@ package com.zaurtregulov.spring.spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean created");
+//        this.pet = pet;
+//    }
+
+
+    public Person() {
         System.out.println("Person bean created");
-        this.pet = pet;
     }
 
     public void callPet(){
         System.out.println("Hey");
         pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Set pet");
+        this.pet = pet;
     }
 }
