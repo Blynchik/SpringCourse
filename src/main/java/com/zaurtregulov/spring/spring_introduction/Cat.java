1373225674d3656322ae9f9aa90f910d005cb7b7
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-30 13:02:42 +0300

    1.14

--- src/main/java/com/zaurtregulov/spring/spring_introduction/Cat.java
+++ src/main/java/com/zaurtregulov/spring/spring_introduction/Cat.java
@@ -1,6 +1,14 @@
 package com.zaurtregulov.spring.spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component("catBean")
 public class Cat implements Pet {
+
+    public Cat() {
+        System.out.println("Cat constructor");
+    }
+
     @Override
     public void say() {
         System.out.println("Meow");
